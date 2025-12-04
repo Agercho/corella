0009d22c6594ab248411ed7fcf2739d30c10b9f2
German Aviles <german.aviles06@gmail.com>
2025-12-04 09:25:31 -0500

    feat: eliminación de valor no usado

--- packages/core-ui/src/styles/getButtonClasses.ts
+++ packages/core-ui/src/styles/getButtonClasses.ts
@@ -1,4 +1,4 @@
-import { clsx, type ClassValue } from "clsx";
+import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export type ButtonSize = "xsmall" | "small" | "medium" | "large" | "xlarge";
