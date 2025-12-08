004ceb12e2140ee4e2d97658c00734f97da691ca
German Aviles <german.aviles06@gmail.com>
2025-12-08 18:46:51 -0500

    feat: exportando los nuevos componentes
    card y timeline

--- packages/astro/src/index.js
+++ packages/astro/src/index.js
@@ -3,3 +3,6 @@ export { default as FilterChip } from "./FilterChip.astro";
 export { default as Input } from "./Input.astro";
 export { default as Checkbox } from "./Checkbox.astro";
 export { default as Radio } from "./Radio.astro";
+export { default as Card } from "./Card.astro";
+export { default as Timeline } from "./Timeline.astro";
+export { default as TimelineItem } from "./TimelineItem.astro";
